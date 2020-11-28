1ad41bb7b50f740a5262935237334e28e57b41f1
Anatoliy Pokhylko <pokhilko.av@gmail.com>
2020-11-28 15:57:05 +0200

    feat: main layout products

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,277 @@ import React from 'react';
 import './App.scss';
 
 export const App = () => (
-  <div className="App">
-    The food
+  <div className="content">
+    <div className="container">
+      <div className="content__filter">
+        <div className="categories">
+          <ul className="categories__list">
+            <li className="categories__item active">All</li>
+            <li className="categories__item">Our Favourites</li>
+            <li className="categories__item">Beef</li>
+            <li className="categories__item">Chicken</li>
+            <li className="categories__item">Pork</li>
+          </ul>
+        </div>
+        <div className="sort">
+          <div className="sort__label">
+            Sort by:
+            <span>best selling</span>
+          </div>
+          <div className="sort__popup">
+            <ul className="sort__list">
+              <li className="sort__item active">best selling</li>
+              <li className="sort__item">price</li>
+              <li className="sort__item">alphabetically</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <h2 className="content__title">All burgers</h2>
+      <div className="content__items">
+        <div className="product">
+          <img
+            className="product__image"
+            src="https://theburger.ua/wp-content/uploads/Arkansas-min-1.jpg"
+            alt="burger"
+          />
+          <h4 className="product__title">«Arkansas»</h4>
+          <div className="product__selector selector">
+            <ul className="selector__type">
+              <li className="selector__item active">classic</li>
+              <li className="selector__item">vegan</li>
+            </ul>
+            <ul className="selector__type">
+              <li className="selector__item active">M</li>
+              <li className="selector__item">L</li>
+              <li className="selector__item">XL</li>
+            </ul>
+          </div>
+          <div className="product__bottom">
+            <div className="product__price">from £25.00</div>
+            <div className="button button--black button--add">
+              Add to cart
+              <span>2</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="product">
+          <img
+            className="product__image"
+            src="https://theburger.ua/wp-content/uploads/Arkansas-min-1.jpg"
+            alt="burger"
+          />
+          <h4 className="product__title">«Arkansas»</h4>
+          <div className="product__selector selector">
+            <ul className="selector__type">
+              <li className="selector__item active">classic</li>
+              <li className="selector__item">vegan</li>
+            </ul>
+            <ul className="selector__type">
+              <li className="selector__item active">M</li>
+              <li className="selector__item">L</li>
+              <li className="selector__item">XL</li>
+            </ul>
+          </div>
+          <div className="product__bottom">
+            <div className="product__price">from £25.00</div>
+            <div className="button button--black button--add">
+              Add to cart
+              <span>2</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="product">
+          <img
+            className="product__image"
+            src="https://theburger.ua/wp-content/uploads/Arkansas-min-1.jpg"
+            alt="burger"
+          />
+          <h4 className="product__title">«Arkansas»</h4>
+          <div className="product__selector selector">
+            <ul className="selector__type">
+              <li className="selector__item active">classic</li>
+              <li className="selector__item">vegan</li>
+            </ul>
+            <ul className="selector__type">
+              <li className="selector__item active">M</li>
+              <li className="selector__item">L</li>
+              <li className="selector__item">XL</li>
+            </ul>
+          </div>
+          <div className="product__bottom">
+            <div className="product__price">from £25.00</div>
+            <div className="button button--black button--add">
+              Add to cart
+              <span>2</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="product">
+          <img
+            className="product__image"
+            src="https://theburger.ua/wp-content/uploads/Arkansas-min-1.jpg"
+            alt="burger"
+          />
+          <h4 className="product__title">«Arkansas»</h4>
+          <div className="product__selector selector">
+            <ul className="selector__type">
+              <li className="selector__item active">classic</li>
+              <li className="selector__item">vegan</li>
+            </ul>
+            <ul className="selector__type">
+              <li className="selector__item active">M</li>
+              <li className="selector__item">L</li>
+              <li className="selector__item">XL</li>
+            </ul>
+          </div>
+          <div className="product__bottom">
+            <div className="product__price">from £25.00</div>
+            <div className="button button--black button--add">
+              Add to cart
+              <span>2</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="product">
+          <img
+            className="product__image"
+            src="https://theburger.ua/wp-content/uploads/Arkansas-min-1.jpg"
+            alt="burger"
+          />
+          <h4 className="product__title">«Arkansas»</h4>
+          <div className="product__selector selector">
+            <ul className="selector__type">
+              <li className="selector__item active">classic</li>
+              <li className="selector__item">vegan</li>
+            </ul>
+            <ul className="selector__type">
+              <li className="selector__item active">M</li>
+              <li className="selector__item">L</li>
+              <li className="selector__item">XL</li>
+            </ul>
+          </div>
+          <div className="product__bottom">
+            <div className="product__price">from £25.00</div>
+            <div className="button button--black button--add">
+              Add to cart
+              <span>2</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="product">
+          <img
+            className="product__image"
+            src="https://theburger.ua/wp-content/uploads/Arkansas-min-1.jpg"
+            alt="burger"
+          />
+          <h4 className="product__title">«Arkansas»</h4>
+          <div className="product__selector selector">
+            <ul className="selector__type">
+              <li className="selector__item active">classic</li>
+              <li className="selector__item">vegan</li>
+            </ul>
+            <ul className="selector__type">
+              <li className="selector__item active">M</li>
+              <li className="selector__item">L</li>
+              <li className="selector__item">XL</li>
+            </ul>
+          </div>
+          <div className="product__bottom">
+            <div className="product__price">from £25.00</div>
+            <div className="button button--black button--add">
+              Add to cart
+              <span>2</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="product">
+          <img
+            className="product__image"
+            src="https://theburger.ua/wp-content/uploads/Arkansas-min-1.jpg"
+            alt="burger"
+          />
+          <h4 className="product__title">«Arkansas»</h4>
+          <div className="product__selector selector">
+            <ul className="selector__type">
+              <li className="selector__item active">classic</li>
+              <li className="selector__item">vegan</li>
+            </ul>
+            <ul className="selector__type">
+              <li className="selector__item active">M</li>
+              <li className="selector__item">L</li>
+              <li className="selector__item">XL</li>
+            </ul>
+          </div>
+          <div className="product__bottom">
+            <div className="product__price">from £25.00</div>
+            <div className="button button--black button--add">
+              Add to cart
+              <span>2</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="product">
+          <img
+            className="product__image"
+            src="https://theburger.ua/wp-content/uploads/Arkansas-min-1.jpg"
+            alt="burger"
+          />
+          <h4 className="product__title">«Arkansas»</h4>
+          <div className="product__selector selector">
+            <ul className="selector__type">
+              <li className="selector__item active">classic</li>
+              <li className="selector__item">vegan</li>
+            </ul>
+            <ul className="selector__type">
+              <li className="selector__item active">M</li>
+              <li className="selector__item">L</li>
+              <li className="selector__item">XL</li>
+            </ul>
+          </div>
+          <div className="product__bottom">
+            <div className="product__price">from £25.00</div>
+            <div className="button button--black button--add">
+              Add to cart
+              <span>2</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="product">
+          <img
+            className="product__image"
+            src="https://theburger.ua/wp-content/uploads/Arkansas-min-1.jpg"
+            alt="burger"
+          />
+          <h4 className="product__title">«Arkansas»</h4>
+          <div className="product__selector selector">
+            <ul className="selector__type">
+              <li className="selector__item active">classic</li>
+              <li className="selector__item">vegan</li>
+            </ul>
+            <ul className="selector__type">
+              <li className="selector__item active">M</li>
+              <li className="selector__item">L</li>
+              <li className="selector__item">XL</li>
+            </ul>
+          </div>
+          <div className="product__bottom">
+            <div className="product__price">from £25.00</div>
+            <div className="button button--black button--add">
+              Add to cart
+              <span>2</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 );
